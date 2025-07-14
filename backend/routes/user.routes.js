f32ec05345a41c12f591e748878044cb84720a97
prabhatkrdprr24 <prabhatkrdprr24@gmail.com>
2025-07-15 02:59:43 +0530

    adding user profile backend api

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { getCurrentUser, suggestedUsers } from '../controllers/user.controllers.js';
+import { getCurrentUser, getProfile, suggestedUsers } from '../controllers/user.controllers.js';
 import isAuth from '../middlewares/isAuth.js';
+import { upload } from '../middlewares/multer.js';
+import { editProfile } from '../controllers/user.controllers.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/current', isAuth, getCurrentUser);
 userRouter.get('/suggested', isAuth, suggestedUsers);
+userRouter.post('/editProfile', isAuth, upload.single("profileImage"), editProfile);
+userRouter.post('/getProfile/:username', isAuth, getProfile);
 
 export default userRouter;
