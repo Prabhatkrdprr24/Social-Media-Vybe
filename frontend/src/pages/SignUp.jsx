ab39f9499df0cbc112a77f2059627d03a39c103d
prabhatkrdprr24 <prabhatkrdprr24@gmail.com>
2025-07-14 11:43:54 +0530

    auth backend api code added

---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='w-full h-screen bg-gradient-to-b from-black to gray-900 flex flex-col justify-center items-center'>
+
+      <div>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default SignUp;
